49b0b2ddf576da7ef7789c1b36008ffd72eb7907
rodrigooliveira19 <oliveirarodrigo19@yahoo.com.br>
2021-03-10 19:49:22 -0300

    Implementando autorização

--- src/main/java/com/rodrigo/cursomc/config/SecurityConfig.java
+++ src/main/java/com/rodrigo/cursomc/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.rodrigo.cursomc.security.JWTAuthenticationFilter;
+import com.rodrigo.cursomc.security.JWTAuthorizationFilter;
 import com.rodrigo.cursomc.security.JWTUtil;
 
 @Configuration
@@ -57,6 +58,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		.anyRequest().authenticated(); 
 		
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil)); 
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil,userDetailsService)); 
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS); 
 	}
 	
