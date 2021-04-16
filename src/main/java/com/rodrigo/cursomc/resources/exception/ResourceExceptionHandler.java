205f8d1105e46da088e30b6c95e32d9bb3c3eb8d
rodrigooliveira19 <oliveirarodrigo19@yahoo.com.br>
2021-04-15 21:07:36 -0300

    Tratando excecoes adequadamente

--- src/main/java/com/rodrigo/cursomc/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/rodrigo/cursomc/resources/exception/ResourceExceptionHandler.java
@@ -2,7 +2,6 @@ package com.rodrigo.cursomc.resources.exception;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.apache.tomcat.util.http.parser.Authorization;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -10,14 +9,18 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.services.s3.model.AmazonS3Exception;
 import com.rodrigo.cursomc.exception.AuthorizationException;
 import com.rodrigo.cursomc.exception.DataIntegrityException;
+import com.rodrigo.cursomc.exception.FileException;
 import com.rodrigo.cursomc.exception.ObjectNotFoundException;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
 	
-	//@ExceptionHandler - O spring já fica na escuta para este tipo de exceçao. O Parametro dafunção recebe a exceção. 
+	//@ExceptionHandler - O spring já fica na escuta para este tipo de exceçao. O Parametro da função recebe a exceção. 
 	@ExceptionHandler(ObjectNotFoundException.class)
 	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException ex, HttpServletRequest request){
 		
@@ -48,6 +51,37 @@ public class ResourceExceptionHandler {
 		//FORBIDDEN - código para acesso negado.
 		StandardError err = new StandardError(HttpStatus.FORBIDDEN.value(), ex.getMessage(), System.currentTimeMillis());
 		return ResponseEntity.status(HttpStatus.FORBIDDEN).body(err); 
+	}
+	
+	@ExceptionHandler(FileException.class)
+	public ResponseEntity<StandardError> file(FileException ex, HttpServletRequest request){
+		
+		StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), ex.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err); 
+	}
+	
+	@ExceptionHandler(AmazonServiceException.class)
+	public ResponseEntity<StandardError> amazonService(AmazonServiceException ex, HttpServletRequest request){
+		
+		//Pegar o status http da Excecao / AWS Error code. 
+		HttpStatus code = HttpStatus.valueOf(ex.getErrorCode()); 
+		
+		StandardError err = new StandardError(code.value(), ex.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(code).body(err); 
+	}
+	
+	@ExceptionHandler(AmazonClientException.class)
+	public ResponseEntity<StandardError> amazoClient(AmazonClientException ex, HttpServletRequest request){
+		
+		StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), ex.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err); 
+	}
+	
+	@ExceptionHandler(AmazonS3Exception.class)
+	public ResponseEntity<StandardError> amazoClient(AmazonS3Exception ex, HttpServletRequest request){
+		
+		StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), ex.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err); 
 	}
 
 }
